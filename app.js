72a7d6c97de5bdcfb9f3cd72e157ef89acd4f8db
Md Mamun <juborajvai22@gmail.com>
2022-11-28 00:50:29 +0600

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 app.use(cors({
-      // origin: "https://course-commerse.web.app"
+      origin: "http://localhost:3000"
 }))
 // const cookieParser = require('cookie-parser')
 const fileUpload = require("express-fileupload");
