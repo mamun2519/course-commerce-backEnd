e9ab487efa09194b0b664f884121ffb2d324ebcb
Md Mamun <juborajvai22@gmail.com>
2022-12-03 19:09:34 +0600

    upate

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const paypal = require('paypal-rest-sdk');
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
   })
 );
 // const cookieParser = require('cookie-parser')....
